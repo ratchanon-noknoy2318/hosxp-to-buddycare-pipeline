3a0b09db9cc8cd27a332f571df7531e864fa7300
Ratchanon Noknoy <ratchanon.noknoy2318@gmail.com>
2026-01-30 17:34:55 +0700

    Update extract_village_list.sql

--- SQL/extract_village_list.sql
+++ SQL/extract_village_list.sql
@@ -1,6 +1,7 @@
 SELECT
     vl.village_moo,
-    CONCAT(LEFT(vl.village_name, 3), '****') AS village_name,
-    CONCAT('****', RIGHT(ta.full_name, 10)) AS full_name
+    CONCAT(LEFT(vl.village_name, 3), '****') AS village_name_mask,
+    CONCAT('****', RIGHT(ta.full_name, 10)) AS full_name_mask
 FROM village vl
-LEFT JOIN thaiaddress ta ON ta.addressid = vl.address_id;
+LEFT JOIN thaiaddress ta
+    ON ta.addressid = vl.address_id;
